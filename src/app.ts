67557e9d17960a9600e18e1abe01d7ea11b1fd82
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-22 16:54:05 +0600

    route and error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,45 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 const app = express()
 
 // parsers
 app.use(express.json())
 
-app.get('/', (req : Request , res : Response) => {
-    res.send('Hello World Server Test joy bangla!')
+// middle wares
+const someData = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        console.log("from middle war", req.hostname);
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Best Practice server!')
 })
-app.post("/",async(req:Request, res:Response)=>{
-    console.log(req.body)
+app.get("/test", someData, async (req: Request, res: Response) => {
     res.send('post data send')
 })
 
-export default app ;
+// global route error handler
+app.all('*', (req: Request, res: Response) => {
+    res.status(400).json({
+        success: false,
+        message: "Route not found"
+    })
+})
+
+
+// global error handler
+type Err = string | number | undefined | null;
+app.use((error: Err, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "something went wrong"
+        })
+    }
+})
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import { Server } from "http";
 import app from "./app"
 const port = 3000;
 
-
 async function bootstrap() {
-    const server: Server = app.listen(port, () => {
-        console.log("database connect is not yet")
+    app.listen(port, () => {
+        console.log("database connect is not yet!")
         console.log(`Example app listening on port ${port}`)
     })
 }
