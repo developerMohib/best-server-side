9aaa776bf95e35ecb2c20bbff4204305e9b2255c
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-25 02:16:19 +0600

    interface and schema added

---
+++ src/modules/students/student.interface.ts
@@ -0,0 +1,38 @@
+// Create an interface representing a document in MongoDB.
+export interface IUserName {
+  firstName: string;
+  midName?: string;
+  lastName: string;
+}
+export interface ILocalGuardian {
+  name: string;
+  occufassion: string;
+  contact: string;
+  address: string;
+}
+
+export interface IGuardian {
+  fatherName: string;
+  fatherProffession: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherProffession: string;
+  motherContactNo: string;
+}
+
+export interface IStudent {
+  id: string;
+  name: IUserName;
+  email: string;
+  image?: string;
+  gender: 'male' | 'female';
+  birthDate?: string;
+  contactNo: string;
+  emargancyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddres: string;
+  permanentAddres: string;
+  guardian: IGuardian;
+  localGuardian: ILocalGuardian;
+  active: 'active' | 'blocked';
+}
---
+++ src/modules/students/student.schema.ts
@@ -0,0 +1,66 @@
+// Create a Schema corresponding to the document interface.
+
+import { Schema, model } from 'mongoose';
+import {
+  IGuardian,
+  ILocalGuardian,
+  IStudent,
+  IUserName,
+} from './student.interface';
+
+// sub schema for user name
+const userSchema = new Schema<IUserName>({
+  firstName: { type: String, required: true },
+  midName: { type: String },
+  lastName: { type: String, require: true },
+});
+
+// sub schema for guardian
+const guardianSchema = new Schema<IGuardian>({
+  fatherContactNo: {
+    type: String,
+    require: true,
+  },
+  fatherProffession: {
+    type: String,
+    require: true,
+  },
+  fatherName: {
+    type: String,
+    require: true,
+  },
+  motherContactNo: { type: String, require: true },
+  motherName: { type: String, require: true },
+  motherProffession: { type: String, require: true },
+});
+
+// sub schema for local guardian
+const localGuardianSchema = new Schema<ILocalGuardian>({
+  name: { type: String, require: true },
+  occufassion: { type: String, require: true },
+  contact: { type: String, require: true },
+  address: { type: String, require: true },
+});
+
+const studentSchema = new Schema<IStudent>({
+  id: String,
+  name: userSchema,
+  email: { type: String, required: true },
+  image: String,
+  gender: { type: String, enum: ['male', 'female'], required: true },
+  birthDate: String,
+  contactNo: String,
+  emargancyContactNo: String,
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
+  permanentAddres: String,
+  presentAddres: String,
+  active: ['active', 'blocked'],
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+});
+
+// 3. Create a Model.
+export const Student = model<IStudent>('Student', studentSchema);
