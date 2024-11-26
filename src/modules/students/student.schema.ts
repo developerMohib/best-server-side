1939e9ec095a40191d60342e557466d5c0e9dac3
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-27 01:56:39 +0600

    some validation in my code

--- src/modules/students/student.schema.ts
+++ src/modules/students/student.schema.ts
@@ -13,12 +13,25 @@ const userSchema = new Schema<IUserName>({
   firstName: {
     type: String,
     required: [true, 'First Name is required'],
+    trim: true,
     minlength: [2, 'First name minimum length 2'],
     maxlength: [20, 'First name maximun length 20 charecters'],
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
   },
-  midName: { type: String },
+  midName: {
+    type: String,
+    trim: true,
+    maxlength: [20, 'Middle name maximum length 20 charecters'],
+  },
   lastName: {
     type: String,
+    trim: true,
     require: [true, 'Last Name is required'],
     maxlength: [20, 'First name maximun length 20 charecters'],
   },
@@ -28,56 +41,116 @@ const userSchema = new Schema<IUserName>({
 const guardianSchema = new Schema<IGuardian>({
   fatherContactNo: {
     type: String,
+    trim: true,
     require: [true, 'Father Contact Number is required'],
     match: [/^\d{11}$/, 'Contact number have to exactly 11 '],
     unique: true,
   },
   fatherProffession: {
     type: String,
+    trim: true,
     require: true,
+    capitalize: {
+      validator: function (value: string) {
+        const fatherProfStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return fatherProfStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
   },
   fatherName: {
     type: String,
+    trim: true,
     require: true,
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
   },
   motherContactNo: {
     type: String,
     match: [/^\d{11}$/, 'Contact number have to exactly 11 '],
     unique: true,
   },
-  motherName: { type: String, require: true },
-  motherProffession: { type: String, require: true },
+  motherName: {
+    type: String,
+    require: true,
+    trim: true,
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
+  },
+  motherProffession: { type: String, require: true, trim: true },
 });
 
 // sub schema for local guardian
 const localGuardianSchema = new Schema<ILocalGuardian>({
-  name: { type: String, require: true },
-  occufassion: { type: String, require: true },
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
+  },
+  occufassion: {
+    type: String,
+    require: true,
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
+  },
   contact: {
     type: String,
     require: [true, 'Contact is required'],
     match: [/^\d{11}$/, 'Contact number have to exactly 11 '],
+    trim: true,
   },
-  address: { type: String, require: true },
+  address: { type: String, require: true, trim: true },
 });
 
 const studentSchema = new Schema<IStudent>({
   id: {
     type: String,
+    trim: true,
     required: [true, 'id is mandatory'],
     unique: true,
   },
   name: {
     type: userSchema,
+    trim: true,
     required: [true, 'name is required to give'],
+    capitalize: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not capitalize format',
+    },
   },
   email: {
     type: String,
     required: [true, 'Provide a valid email'],
     match: [/^\S+@\S+\.\S+$/, 'Invalid email format'],
     unique: true,
+    trim: true,
   },
-  image: String,
+  image: { type: String, trim: true },
   gender: { type: String, enum: ['male', 'female'], required: true },
   birthDate: String,
   contactNo: {
@@ -96,8 +169,8 @@ const studentSchema = new Schema<IStudent>({
     type: String,
     enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   },
-  permanentAddres: String,
-  presentAddres: String,
+  permanentAddres: { type: String, trim: true },
+  presentAddres: { type: String, trim: true },
   active: { type: String, enum: ['active', 'blocked'], default: 'active' },
   guardian: guardianSchema,
   localGuardian: {
